cd54b41f0e7c569365420c984fb291262c373c07
Daniel <dan.andrade20@gmail.com>
2023-11-21 10:17:39 -0300

    carrossel

--- Sobre nós/script.js
+++ Sobre nós/script.js
@@ -1,30 +1,91 @@
-function trocarTema(){
-    let body = document.body;
+//Troca de tema
 
-    if (body.classList.contains('theme-light')){
-            body.classList.remove('theme-light');
-            body.classList.add('theme-dark');
-    } 
-    else {
-        body.classList.remove('theme-dark');
-        body.classList.add('theme-light');
-    }
+function trocarTema() {
+  let body = document.body;
+
+  if (body.classList.contains("theme-light")) {
+    body.classList.remove("theme-light");
+    body.classList.add("theme-dark");
+  } else {
+    body.classList.remove("theme-dark");
+    body.classList.add("theme-light");
+  }
 }
 
-var formEntrar = document.querySelector('#entrar');
-var formCadastro = document.querySelector('#cadastro');
-var btnColor = document.querySelector('.btnColor');
+//cadastro
 
-document.querySelector('#btnEntrar')
-.addEventListener('click', () =>{
+var formEntrar = document.querySelector("#entrar");
+var formCadastro = document.querySelector("#cadastro");
+var btnColor = document.querySelector(".btnColor");
+var urlCompleta = window.location.href;
+if (urlCompleta.indexOf("loguin") != -1) {
+  document.querySelector("#btnEntrar").addEventListener("click", () => {
     formEntrar.style.left = "25px";
     formCadastro.style.left = "450px";
     btnColor.style.left = "0px";
-} )
+  });
 
-document.querySelector('#btnCadastro')
-.addEventListener('click', () =>{
+  document.querySelector("#btnCadastro").addEventListener("click", () => {
     formEntrar.style.left = "-450px";
     formCadastro.style.left = "25px";
     btnColor.style.left = "110px";
-} )
+  });
+}
+
+//carrocel
+
+window.onload = function () {
+  let btnAvancar = document.getElementById("avancar");
+  let btnVoltar = document.getElementById("voltar");
+  let container = document.getElementById("container");
+  let imgs = document.querySelectorAll("#container img");
+
+  imgs.forEach((element) => {
+    if (element.hasAttribute("data-link")) {
+      element.style.cursor = "pointer";
+      element.onclick = function () {
+        window.open(element.dataset.link, "_blank");
+      };
+    }
+  });
+
+  btnAvancar.addEventListener("click", () => {
+    btnAvancar.disabled = true;
+    btnVoltar.disabled = true;
+    imgs = document.querySelectorAll("#container img");
+    container.insertBefore(imgs.item(imgs.length - 1), container.firstChild);
+    for (img of imgs) {
+      img.style.transition = "0ms";
+      img.style.left = "-250px";
+    }
+    setTimeout(() => {
+      for (img of imgs) {
+        img.style.transition = "250ms";
+        img.style.left = "0px";
+      }
+      btnAvancar.disabled = false;
+      btnVoltar.disabled = false;
+    }, 100);
+  });
+  btnVoltar.addEventListener("click", () => {
+    btnAvancar.disabled = true;
+    btnVoltar.disabled = true;
+
+    for (img of imgs) {
+      img.style.transition = "250ms";
+      img.style.left = "-250px";
+    }
+    imgs = document.querySelectorAll("#container img");
+
+    setTimeout(() => {
+      container.appendChild(imgs.item(0));
+
+      for (img of imgs) {
+        img.style.transition = "0ms";
+        img.style.left = "0px";
+      }
+      btnAvancar.disabled = false;
+      btnVoltar.disabled = false;
+    }, 250);
+  });
+};
